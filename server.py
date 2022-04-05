a7a5c0122b71e5cbd750b35b65a9af23a121c4d4
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-05 22:26:41 +0200

    Implement question dictionary - TODO: Bug reassigns all values in list

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 
+import data_handler
+
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/list")
 def route_list():
-    # TODO create data handler
-    return render_template('list.html')
+    questions = data_handler.get_question_data()
+    return render_template('list.html', questions=questions)
 
 
 if __name__ == "__main__":
