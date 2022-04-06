8d7ce9520a14be466f2af4cd85eedd915dd7fe91
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-06 08:47:49 +0200

    Fix dictionary value assignment from CSV bug

--- data_handler.py
+++ data_handler.py
@@ -6,26 +6,14 @@ DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', '
 
 
 def get_question_data():
-    questions = {}
     result = []
     with open(DATA_FILE_PATH) as csvfile:
-        for row in csvfile:
-            if row.strip() == "id,submission_time,view_number,vote_number,title,message,image":
-                continue
-            else:
-                record = row.split(",")
-                for index, header in enumerate(DATA_HEADER):
-                    questions[header] = record[index]  # Changes all values in list to those defined in dictionary
-                    result.append(questions)
-
-                    # question_dict["id"] = record[0]
-                    # question_dict["submission_time"] = record[1]
-                    # question_dict["view_number"] = record[2]
-                    # question_dict["vote_number"] = record[3]
-                    # question_dict["title"] = record[4]
-                    # question_dict["message"] = record[5]
-                    # question_dict["image"] = record[6]
-                    # result.append(question_dict)
+        lines = [row.strip().split(",") for row in csvfile if DATA_HEADER[0] not in row]
+    for index, entry in enumerate(lines):
+        questions = {}
+        for i, header in enumerate(DATA_HEADER):
+            questions[header] = entry[i]
+        result.append(questions)
     return result
 
 
@@ -33,4 +21,3 @@ if __name__ == '__main__':
     res = get_question_data()
     for line in res:
         print(line)
-
