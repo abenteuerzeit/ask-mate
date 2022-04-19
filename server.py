c3caee287c8af61160914d338e69eda58c51eb34
piotrpalacz <peterpalacz12@gmail.com>
2022-04-19 23:36:58 +0200

    Display question

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import data_handler
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/list")
 def route_list():
-    questions = data_handler.get_question_data()
+    questions = data_handler.get_questions()
     return render_template('list.html', questions=questions)
 
 
@@ -15,17 +15,22 @@ def route_add_question():
     add = data_handler.save_question_data()
     return render_template('add-question.html', add=add)
 
+
 @app.route('/question/<id>')
 def question(id):
-    return render_template('question.html', methods=["GET", "POST"])
+    if request.method == "GET":
+        answers = data_handler.get_answer_for_question(id)
+        return render_template('question.html', question=data_handler.get_question(id), answers=answers)
+
 
 # @app.route("/question/<id>/edit")
 # def edit_question(id):
 #     return render_template('edit-question.html')
 
+
 if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
-        port=8000,
+        port=5000,
         debug=True,
     )
