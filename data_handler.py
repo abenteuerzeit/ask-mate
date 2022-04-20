2f80f2aa71da72aef3197e1bfa992871c644fa64
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-20 16:10:29 +0200

    Add a question

--- data_handler.py
+++ data_handler.py
@@ -12,7 +12,7 @@ def get_questions():
     # with open(Q_FILE_PATH) as csvfile:
     #     lines = [row.strip().split(",") for row in csvfile if Q_HEADER[0] not in row]
     #     for line in lines:
-    #         line[1] = datetime.datetime.fromtimestamp(int(line[1])).strftime('%Y-%m-%d %H:%M:%S')
+    #         line[1] = datetime.fromtimestamp(int(line[1])).strftime('%Y-%m-%d %H:%M:%S')
     # return sorted([{header: entry[index] for index, header in enumerate(Q_HEADER)}
     #                for entry in lines], key=lambda d: d['submission_time'], reverse=True)
     questions = []
@@ -47,19 +47,43 @@ def get_answer_for_question(question_id):
     return answers
 
 
-
-def save_question_data():
-    pass
+def save_question_data(user_input):
+    question_list = get_questions()
+    question_list.append({'id': str(get_new_id()), 'submission_time': int(datetime.now().timestamp()),
+                          'view_number': '0', 'vote_number': '0', 'title': user_input["title"],
+                          'message': user_input["message"], 'image': user_input['image']})
+    with open(Q_FILE_PATH, 'w', newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=Q_HEADER)
+        writer.writeheader()
+        writer.writerows(question_list)
 
 
 def save_answer_data():
     pass
 
 
+def get_new_id():
+    with open(Q_FILE_PATH, 'r') as file:
+        return int(max([q[0] for q in file.readlines() if q[0] != "i"])) + 1
+
+
 if __name__ == '__main__':
-    q = get_answer_data()
-    print(q)
+    data = {}
+    for i in Q_HEADER:
+        data[i] = 'test'
+    save_question_data(data)
+    # q = get_answer_data()
+    # print(q)
+
 
     # a = get_answer_data()
     # for line in a:
     #     print(line)
+
+
+"""
+id,submission_time,view_number,vote_number,title,message,image
+1,1493368154,29,7,"How to make lists in Python? I am totally new to this, any hints?",None
+2,1493068124,15,9,"Wordpress loading multiple jQuery Versions.", "I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $('.myBook').booklet(); I could easy managing the loading order with wp_enqueue_script so first I load jquery then I load booklet so everything is fine. BUT in my theme i also using jquery via webpack so the loading order is now following: jquery booklet app.js (bundled file with webpack including jquery)","images/image1.png"
+3,1493015432,1364,57,"Drawing canvas with an image picked with Cordova Camera Plugin., "I'm getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I'm on IOS it throws errors such as cross origin issue or that I'm trying to use an unknown format. This is the code I'm using to draw the image (that works on web/desktop but not cordova built ios app)",None
+"""
