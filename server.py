50aa3a02b90eaa6f6bf17ff69b825315d8b97de9
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-20 16:10:29 +0200

    Add a question

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import data_handler
 app = Flask(__name__)
 
 
 @app.route("/")
 @app.route("/list")
-def route_list():
+def list_questions():
     questions = data_handler.get_questions()
-    return render_template('list.html', questions=questions)
+    return render_template("list.html", questions=questions)
 
 
-@app.route("/add-question")
-def route_add_question():
-    add = data_handler.save_question_data()
-    return render_template('add-question.html', add=add)
+@app.route("/add-question", methods=["GET", "POST"])
+def add_question():
+    if request.method == "GET":
+        return render_template("add-question.html")
+    elif request.method == "POST":
+        data = {'title': request.form['title'], 'message': request.form['message'], 'image': 'None'}
+        data_handler.save_question_data(data)
+        return render_template("question.html", question=data)
+    return redirect('/')
+
 
 
 @app.route('/question/<id>')
