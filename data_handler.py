ae867cdf3044a4a6438d6f900202e552fc15e0f7
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-21 00:07:28 +0200

    Refactor

--- data_handler.py
+++ data_handler.py
@@ -53,25 +53,30 @@ def get_answer_for_question(question_id):
 
 
 def save_question_data(user_input):
+    question_list = get_questions()
     new_question = {'id': str(get_new_id(Q_FILE_PATH)), 'submission_time': NOW,
                     'view_number': '0', 'vote_number': '0', 'title': user_input['title'],
                     'message': user_input['message'], 'image': user_input['image']}
-    with open(Q_FILE_PATH, 'a', newline=None) as csvfile:
-        writer = DictWriter(csvfile, fieldnames=Q_HEADER)
-        writer.writerow(new_question)
-        csvfile.close()
+    question_list.append(new_question)
+    write_over(Q_FILE_PATH, Q_HEADER, question_list)
     return new_question
 
 
 def save_answer_data(user_input):
+    answer_list = get_answers()
     new_answer = {'id': str(get_new_id(A_FILE_PATH)), 'submission_time': NOW, 'vote_number': '0',
                   'question_id': user_input['question_id'], 'message': user_input['message'],
                   'image': user_input['image']}
-    with open(A_FILE_PATH, 'a', newline=None) as csvfile:
-        writer = DictWriter(csvfile, fieldnames=A_HEADER)
-        writer.writerow(new_answer)
-        csvfile.close()
+    answer_list.append(new_answer)
+    write_over(A_FILE_PATH, A_HEADER, answer_list)
     return new_answer
+
+
+def write_over(file, header, content):
+    with open(file, 'w', newline="") as csvfile:
+        writer = DictWriter(csvfile, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(content)
 
 
 def get_new_id(csvfile):
@@ -90,4 +95,5 @@ id,submission_time,view_number,vote_number,title,message,image
 1,1493368154,29,7,"How to make lists in Python? I am totally new to this, any hints?",None
 2,1493068124,15,9,"Wordpress loading multiple jQuery Versions.", "I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $('.myBook').booklet(); I could easy managing the loading order with wp_enqueue_script so first I load jquery then I load booklet so everything is fine. BUT in my theme i also using jquery via webpack so the loading order is now following: jquery booklet app.js (bundled file with webpack including jquery)","images/image1.png"
 3,1493015432,1364,57,"Drawing canvas with an image picked with Cordova Camera Plugin., "I'm getting an image from device and drawing a canvas with filters using Pixi JS. It works all well using computer to get an image. But when I'm on IOS it throws errors such as cross origin issue or that I'm trying to use an unknown format. This is the code I'm using to draw the image (that works on web/desktop but not cordova built ios app)",None
+
 """
