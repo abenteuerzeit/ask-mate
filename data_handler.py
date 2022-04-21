a4b069fc714ce27c05c2bd45c09ab88c025f50ed
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-21 08:41:46 +0200

    Add date and time to question display page

--- data_handler.py
+++ data_handler.py
@@ -21,10 +21,14 @@ def get_questions():
     return questions
 
 
-def convert_to_datetime(dictionary_list):
-    for dictionary in dictionary_list:
-        dictionary['submission_time'] = datetime.fromtimestamp(int(dictionary['submission_time']))
-    return dictionary_list
+def convert_to_datetime(dictionary_data):
+    if type(dictionary_data) is list:
+        for dictionary in dictionary_data:
+            dictionary['submission_time'] = datetime.fromtimestamp(int(dictionary['submission_time']))
+        return dictionary_data
+    if type(dictionary_data) is dict:
+        dictionary_data['submission_time'] = datetime.fromtimestamp(int(dictionary_data['submission_time']))
+        return dictionary_data
 
 
 def get_question(id):
