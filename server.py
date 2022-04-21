4767a340acc555194c2d8c5a9996873d016fdbb7
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-21 23:20:33 +0200

    Add image to server

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template, request, redirect
-
+import os
 import data_handler
 
+from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = './sample_data/images'
+ALLOWED_EXTENSIONS = {'jpg', 'png'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
@@ -104,6 +111,41 @@ def a_downvote(answer_id):
                 data_handler.decrease_answer_vote(answer)
                 q_id = answer['question_id']
                 return redirect('/question/' + q_id)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+
+@app.route('/upload-image', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return ''' <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method="post" action="/upload-image" enctype="multipart/form-data">
+          <input type="file" name="file">
+          <input type="submit" value="Upload">
+        </form>
+        '''
 
 
 if __name__ == "__main__":
