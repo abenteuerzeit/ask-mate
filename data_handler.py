36b56026d2ca42ed777335def6e0244ce35809c1
abenteuerzeit <amroz.muzyka@gmail.com>
2022-04-30 07:01:21 +0200

    Refactor data handler, move functions to util

--- data_handler.py
+++ data_handler.py
@@ -1,6 +1,7 @@
 import os
 from csv import DictWriter, DictReader
 from datetime import datetime
+import util
 
 Q_FILE_PATH = os.getenv('Q_FILE_PATH') if 'Q_FILE_PATH' in os.environ else './sample_data/question.csv'
 A_FILE_PATH = os.getenv('A_FILE_PATH') if 'A_FILE_PATH' in os.environ else './sample_data/answer.csv'
@@ -11,8 +12,7 @@ C_HEADER = ['id', 'question_id', 'answer_id', 'message', 'submission_time', 'edi
 NOW = datetime.fromtimestamp(int(datetime.now().timestamp()))
 
 
-# QUESTIONS
-
+# -------------------------- QUESTIONS -------------------------- #
 
 def save_new_question_data(user_input):
     question_list = get_questions()
@@ -20,7 +20,7 @@ def save_new_question_data(user_input):
                     'view_number': '0', 'vote_number': '0', 'title': user_input['title'],
                     'message': user_input['message'], 'image': user_input['image']}
     question_list.append(new_question)
-    write_over(Q_FILE_PATH, Q_HEADER, question_list)
+    util.write_over(Q_FILE_PATH, Q_HEADER, question_list)
     return new_question
 
 
@@ -54,7 +54,7 @@ def edit_question(updated_dict):
     for index, q in enumerate(question_list):
         if q['id'] == updated_dict['id']:
             question_list[index] = original_question
-    write_over(Q_FILE_PATH, Q_HEADER, question_list)
+    util.write_over(Q_FILE_PATH, Q_HEADER, question_list)
 
 
 def delete_question(id):
@@ -62,7 +62,10 @@ def delete_question(id):
     for index, question_dictionary in enumerate(question_list):
         if question_dictionary['id'] == id:
             del question_list[index]
-    write_over(Q_FILE_PATH, Q_HEADER, question_list)
+    util.write_over(Q_FILE_PATH, Q_HEADER, question_list)
+
+
+# -------------------------- ANSWERS -------------------------- #
 
 
 def get_answers():
@@ -85,6 +88,39 @@ def get_answer_for_question(question_id):
     return answers
 
 
+def save_answer_data(user_input):
+    answer_list = get_answers()
+    new_answer = {'id': str(util.get_new_id(A_FILE_PATH)), 'submission_time': NOW, 'vote_number': '0',
+                  'question_id': user_input['question_id'], 'message': user_input['message'],
+                  'image': user_input['image']}
+    answer_list.append(new_answer)
+    util.write_over(A_FILE_PATH, A_HEADER, answer_list)
+    return new_answer
+
+
+def delete_answer(id):
+    answer_list = get_answers()
+    for index, answer_dictionary in enumerate(answer_list):
+        if answer_dictionary['id'] == id:
+            del answer_list[index]
+    util.write_over(A_FILE_PATH, A_HEADER, answer_list)
+
+
+# -------------------------- COMMENTS -------------------------- #
+
+def save_new_comment(user_input):
+    comment_list = get_comments()
+    new_comment = {'id': str(util.get_new_id(C_FILE_PATH)),
+                   'question_id': str(util.get_new_id(Q_FILE_PATH)),
+                   'answer_id': str(util.get_new_id(A_FILE_PATH)),
+                   'message': user_input['message'],
+                   'submission_time': NOW,
+                   'edited_count': '0'}
+    comment_list.append(new_comment)
+    util.write_over(C_FILE_PATH, C_HEADER, comment_list)
+    return new_comment
+
+
 def get_comments():
     comments = []
     with open(C_FILE_PATH) as csvfile:
@@ -105,6 +141,9 @@ def get_comment_for_question(answer_id):
     return comments
 
 
+# -------------------------- VOTES -------------------------- #
+
+
 def increase_question_view_count(select_qdict):
     # pass question id instead of the whole dictionary
     if select_qdict is not None:
@@ -112,7 +151,7 @@ def increase_question_view_count(select_qdict):
         for question_dictionary in question_list:
             if question_dictionary.get('id') == str(select_qdict['id']):
                 question_dictionary['view_number'] = question_dictionary.get('view_number') + 1
-                write_over(Q_FILE_PATH, Q_HEADER, question_list)
+                util.write_over(Q_FILE_PATH, Q_HEADER, question_list)
 
 
 def increase_question_vote(selected_dictionary):
@@ -120,7 +159,7 @@ def increase_question_vote(selected_dictionary):
     for q_dict in question_list:
         if q_dict.get('id') == str(selected_dictionary['id']):
             q_dict['vote_number'] = q_dict.get('vote_number') + 1
-            write_over(Q_FILE_PATH, Q_HEADER, question_list)
+            util.write_over(Q_FILE_PATH, Q_HEADER, question_list)
 
 
 def increase_answer_vote(selected_dictionary):
@@ -128,7 +167,7 @@ def increase_answer_vote(selected_dictionary):
     for a_dict in answers:
         if a_dict.get('id') == str(selected_dictionary['id']):
             a_dict['vote_number'] = a_dict.get('vote_number') + 1
-            write_over(A_FILE_PATH, A_HEADER, answers)
+            util.write_over(A_FILE_PATH, A_HEADER, answers)
 
 
 def decrease_question_vote(selected_dictionary):
@@ -136,7 +175,7 @@ def decrease_question_vote(selected_dictionary):
     for q_dict in question_list:
         if q_dict.get('id') == str(selected_dictionary['id']):
             q_dict['vote_number'] = q_dict.get('vote_number') - 1
-            write_over(Q_FILE_PATH, Q_HEADER, question_list)
+            util.write_over(Q_FILE_PATH, Q_HEADER, question_list)
 
 
 def decrease_answer_vote(selected_dictionary):
@@ -144,47 +183,4 @@ def decrease_answer_vote(selected_dictionary):
     for a_dict in answers:
         if a_dict.get('id') == str(selected_dictionary['id']):
             a_dict['vote_number'] = a_dict.get('vote_number') - 1
-            write_over(A_FILE_PATH, A_HEADER, answers)
-
-
-def save_new_comment(user_input):
-    comment_list = get_comments()
-    new_comment = {'id': str(get_new_id(C_FILE_PATH)),
-                   'question_id': str(get_new_id(Q_FILE_PATH)),
-                   'answer_id': str(get_new_id(A_FILE_PATH)),
-                   'message': user_input['message'],
-                   'submission_time': NOW,
-                   'edited_count': '0'}
-    comment_list.append(new_comment)
-    write_over(C_FILE_PATH, C_HEADER, comment_list)
-    return new_comment
-
-
-def save_answer_data(user_input):
-    answer_list = get_answers()
-    new_answer = {'id': str(get_new_id(A_FILE_PATH)), 'submission_time': NOW, 'vote_number': '0',
-                  'question_id': user_input['question_id'], 'message': user_input['message'],
-                  'image': user_input['image']}
-    answer_list.append(new_answer)
-    write_over(A_FILE_PATH, A_HEADER, answer_list)
-    return new_answer
-
-
-def write_over(file, header, content):
-    with open(file, 'w', newline="") as csvfile:
-        writer = DictWriter(csvfile, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(content)
-
-
-def get_new_id(csvfile):
-    with open(csvfile, 'r') as file:
-        return len(file.readlines())
-
-
-def delete_answer(id):
-    answer_list = get_answers()
-    for index, answer_dictionary in enumerate(answer_list):
-        if answer_dictionary['id'] == id:
-            del answer_list[index]
-    write_over(A_FILE_PATH, A_HEADER, answer_list)
+            util.write_over(A_FILE_PATH, A_HEADER, answers)
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from csv import DictWriter, DictReader
 
 
 def convert_to_datetime(dictionary_data):
@@ -9,3 +10,15 @@ def convert_to_datetime(dictionary_data):
     if type(dictionary_data) is dict:
         dictionary_data['submission_time'] = datetime.fromtimestamp(int(dictionary_data['submission_time']))
         return dictionary_data
+
+
+def write_over(file, header, content):
+    with open(file, 'w', newline="") as csvfile:
+        writer = DictWriter(csvfile, fieldnames=header)
+        writer.writeheader()
+        writer.writerows(content)
+
+
+def get_new_id(csvfile):
+    with open(csvfile, 'r') as file:
+        return len(file.readlines())
